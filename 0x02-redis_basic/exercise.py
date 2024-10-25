816650537560bdb3089a4f986f6ea6016a9feaee
scodeq65 <sodiqagbaraojo@gmail.com>
2024-10-25 09:01:44 +0000

    reading and recovering from redis

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python3
 """
-Cache module for storing data in Redis with random keys.
+Cache module for storing and retrieving data in Redis with random keys.
 """
 
 import redis
 import uuid
-from typing import Union
+from typing import Union, Callable, Optional
 
 class Cache:
     """Cache class for storing data with unique keys in Redis."""
@@ -28,3 +28,49 @@ class Cache:
         key = str(uuid.uuid4())
         self._redis.set(key, data)
         return key
+
+    def get(
+        self, key: str, fn: Optional[Callable] = None
+    ) -> Optional[Union[str, bytes, int, float]]:
+        """
+        Retrieve data from Redis by key and optionally
+        convert it using a callable.
+
+        Parameters:
+        - key (str): The key under which the data is stored.
+        - fn (Optional[Callable]): A callable function
+        to convert data.
+
+        Returns:
+        - Optional[Union[str, bytes, int, float]]:
+        The retrieved data, converted if fn is provided.
+        """
+        data = self._redis.get(key)
+        if data is None:
+            return None
+        return fn(data) if fn else data
+
+    def get_str(self, key: str) -> Optional[str]:
+        """
+        Retrieve data as a UTF-8 string.
+
+        Parameters:
+        - key (str): The key under which the data is stored.
+
+        Returns:
+        - Optional[str]: The retrieved data as a string.
+        """
+        return self.get(key, fn=lambda d: d.decode("utf-8"))
+
+    def get_int(self, key: str) -> Optional[int]:
+        """
+        Retrieve data as an integer.
+
+        Parameters:
+        - key (str): The key under which the data is stored.
+
+        Returns:
+        - Optional[int]: The retrieved data as an integer.
+        """
+        return self.get(key, fn=int)
+
